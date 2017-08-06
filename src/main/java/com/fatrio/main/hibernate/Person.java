ab6b5239241af15e71f73fecca5008c7402eb663
Fernando Atrio <fernandoatrio@gmail.com>
2017-08-06 03:48:10 -0300

    First commit!!

---
+++ src/main/java/com/fatrio/main/hibernate/Person.java
@@ -0,0 +1,65 @@
+package com.fatrio.main.hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+@Entity(name="person")
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	@Column
+	private String email;
+	
+	public String toString() {
+		ToStringBuilder builder = new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+									.append(id)
+									.append(firstName)
+									.append(lastName)
+									.append(email);
+		return builder.build();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
---
+++ src/test/java/com/fatrio/test/hibernate/TestEntityManager.java
@@ -0,0 +1,81 @@
+package com.fatrio.test.hibernate;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.FlushModeType;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.junit.Test;
+
+import com.fatrio.main.hibernate.Person;
+
+public class TestEntityManager {
+	
+	static EntityManagerFactory factory = Persistence.createEntityManagerFactory( "com.fatrio.test.hibernate" );
+
+	@Test
+	public void testSelectOnePerson() {
+		
+		assertThat(factory, notNullValue());
+		
+		EntityManager entityManager = factory.createEntityManager();
+		
+		entityManager.getTransaction().begin();		
+		CriteriaBuilder criteria = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Person> q = criteria.createQuery(Person.class);
+		Root<Person> root = q.from(Person.class);
+		q.where(criteria.equal(root.get("email"), "PepePe@gmail.com"));
+		
+		Person person = entityManager.createQuery(q).getSingleResult();
+		
+		entityManager.getTransaction().commit();
+		
+		System.out.println(person);
+	}
+	
+	@Test
+	public void testSaveSeveralPeople() {
+		EntityManager manager = factory.createEntityManager();
+		manager.setFlushMode(FlushModeType.COMMIT);
+		
+		Person p1 = new Person();
+		p1.setEmail("p1@gmail.com");
+		p1.setFirstName("p1.firstName");
+		p1.setLastName("p1.lastName");
+		
+		Person p2 = new Person();
+		p2.setEmail("p2@gmail.com");
+		p2.setFirstName("p2.firstName");
+		p2.setLastName("p2.lastName");
+		
+		Person p3 = new Person();
+		p3.setEmail("p3@gmail.com");
+		p3.setFirstName("p3.firstName");
+		p3.setLastName("p3.lastName");
+		
+		manager.getTransaction().begin();
+		manager.persist(p1);
+		manager.persist(p2);
+		manager.persist(p3);
+
+		System.out.println("* Before closing transaction:");
+		for (Person p : Arrays.asList(p1, p2, p3)) {
+			System.out.println(p);
+		}
+		
+		manager.getTransaction().commit();
+		
+		System.out.println("* After closing transaction:");
+		for (Person p : Arrays.asList(p1, p2, p3)) {
+			System.out.println(p);
+		}
+	}
+}
